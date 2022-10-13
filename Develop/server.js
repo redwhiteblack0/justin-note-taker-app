0c484012dcfc009e1df9fb611e1dac66eeacd9c5
Justin Moody <Justindmoody@yahoo.com>
2022-10-13 11:51:12 -0400

    Routes, requests, and responses created

---
+++ Develop/server.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
+const {v4} = require("uuid")
+const app = express()
+
+
+app.use(express.json())
+
+app.get("/notes", (request, response) => {
+    console.log(request)
+    response.sendFile(path.join(__dirname, "public/notes.html"))
+})
+// /notes is a route
+app.get("/api/notes", (request, response) => {
+    const data = fs.readFileSync(path.join(__dirname, "db/db.json"), {
+        encoding: "utf8"
+    })
+    console.log(data)
+    response.json(data)
+})
+
+app.post("/api/notes", (request, response) => {
+    const body = request.body
+    console.log(body)
+    const data = fs.readFileSync(path.join(__dirname, "db/db.json"), {
+        encoding: "utf8"
+    })
+    const parsedData = JSON.parse(data)
+    const newNote = {
+        ...body, 
+        "id": v4()
+    }
+    parsedData.push(newNote)
+    fs.writeFileSync(path.join(__dirname, "db/db.json"), JSON.stringify(parsedData))
+    console.log(parsedData)
+    response.json(newNote)
+})
+
+
+app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "public/index.html"))
+})
+
+app.listen(3000, () => console.log("Server is running"));
+
